adae5e41456165b0f8d5f0d7bb15f070f7827f0a
张君正 <junzheng.zhang@highsoft.ltd>
2022-12-05 11:51:31 +0800

    test: first push to github

--- src/main/java/Entrance.java
+++ src/main/java/Entrance.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.io.*;
 import java.util.ResourceBundle;
 
@@ -5,6 +6,7 @@ public class Entrance {
     public static void main(String[] args) throws IOException {
 
         System.out.print(new Movie("xxx", 1));
+        System.out.print("hello github");
 //        FileWriter fileWriter = new FileWriter("xxx.txt");
 //        fileWriter.write(customer.statement());
 //        fileWriter.close();
